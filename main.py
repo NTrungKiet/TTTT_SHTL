8974d59ab52e65a685219410fb1f980556ec7dd8
NTrungKiet <gamelatuyet@gmail.com>
2023-06-05 12:24:07 +0700

    OCR

--- main.py
+++ main.py
@@ -31,17 +31,13 @@ class OCR:
 
     def sort(self, boxs):
         for i in range(0, len(boxs[0])):
-            # print('lần lặp {}'.format(i+1))
-            # print('boxs là ', boxs[0])
             for j in range(i+1, len(boxs[0])):
                 box_i = np.array(boxs[0][i], dtype=np.int32)
-                # print('min hiện tại là ', min(box_i[:,1]))
                 box_j = np.array(boxs[0][j], dtype=np.int32)
                 if(min(box_i[:,1])>min(box_j[:,1])):
                     temp = boxs[0][i]  
                     boxs[0][i]=boxs[0][j]
                     boxs[0][j]=temp
-                    # print('đổi ', min(box_i[:,1]), ' cho ', min(box_j[:,1]))
         return boxs
 
     def text_partition(self, boxs):
@@ -60,110 +56,109 @@ class OCR:
             # trường hợp nhóm các dòng văn bản:
             # 1.lấy top của tọa độ hiện tại trừ cho bottom của tọa độ trước đó < 50.
             # 2. top bằng nhau hoặc sắp sĩ bằng nhau .
-            # print('Lần thứ {}'.format(i))
-            # print('TH1: ','TOP = ',min(current_box[:, 1]),' Bottom = ', max(previous_box[:, 1]), '=> ',min(current_box[:, 1]) - max(previous_box[:, 1]))
             if((min(current_box[:, 1]) - max(previous_box[:, 1])) < 30):
-                flag = 1
-            # print('TH2: ','Current_TOP = ',min(current_box[:, 1]),' Previous_Top = ', min(previous_box[:, 1]), '=> ',min(current_box[:,1]) - min(previous_box[:,1]))
-            elif((min(current_box[:,1]) - min(previous_box[:,1])) <= 3 and (min(current_box[:,1]) - min(previous_box[:,1])) >=0):
-                flag = 1
-            if (flag == 0):
-                j+=1
-                new_boxs[j]=current_box
-                # new_boxs.append(current_box.tolist())
-            else:
-                # print("previous_box = ",  previous_box)
-                # print("current_box = ",  current_box)
                 x_left = min(min(previous_box[:,0]), min(current_box[:,0])) 
                 x_right = max(max(previous_box[:,0]), max(current_box[:,0]))
                 y_top = min(min(previous_box[:,1]), min(current_box[:,1]))
                 y_bottom = max(max(previous_box[:,1]), max(current_box[:,1]))
                 new_boxs[j]=[[x_left,y_top], [x_right,y_top], [x_right, y_bottom], [x_left, y_bottom]]
-                # print('box mới ', [[x_left,y_top], [x_right,y_top], [x_right, y_bottom], [x_left, y_bottom]])
-                # new_boxs.append([[x_left,y_top], [x_right,y_top], [x_right, y_bottom], [x_left, y_bottom]])
+                flag = 1
+            elif (flag == 0):
+                j+=1
+                new_boxs[j]=current_box
             i+=1
-
         new_boxs = np.delete(new_boxs, list(range(j+1, max_length)), axis = 0)
-        # print(len(new_boxs))
         return new_boxs.tolist()
 
-    def _ocr_from_pdf(self, pdf_path):
+    def ocr_by_vietocr(self, cropped_img):
         results = []
+        boxs_of_ocr=self.paddle.ocr(cropped_img,cls=False, det=True, rec=False)
+        boxs_of_ocr = self.sort(boxs_of_ocr)
+        # pre_img = Image.fromarray(cropped_img)  
+        # draw123 = ImageDraw.Draw(pre_img) 
+        for box_of_ocr in boxs_of_ocr[0]:         
+
+            # for i in range(0, len(box_of_ocr)):
+            #     draw123.line(box_of_ocr[i] + (box_of_ocr[(i + 1) % len(box_of_ocr)]), fill='red', width=3)
+            points_of_ocr = np.array(box_of_ocr, dtype=np.int32)
+            # Chuẩn hóa tọa độ :
+            # x bên trái:
+            points_of_ocr[0][0] = points_of_ocr[3][0] = min(points_of_ocr[0][0], points_of_ocr[3][0]) 
+            # y trên:
+            points_of_ocr[0][1] = points_of_ocr[1][1] = min(points_of_ocr[0][1], points_of_ocr[1][1]) 
+            # x bên phải:
+            points_of_ocr[1][0] = points_of_ocr[2][0] = max(points_of_ocr[1][0], points_of_ocr[2][0])
+            # y dưới:
+            points_of_ocr[2][1] = points_of_ocr[3][1] = max(points_of_ocr[2][1], points_of_ocr[3][1]) 
+            # print(box_of_ocr, '\n => \n', points_of_ocr)         
+                            
+
+            width_of_ocr = int(np.sqrt((points_of_ocr[1][0] - points_of_ocr[0][0])**2 + (points_of_ocr[1][1] - points_of_ocr[0][1])**2))
+            height_of_ocr = int(np.sqrt((points_of_ocr[2][0] - points_of_ocr[1][0])**2 + (points_of_ocr[2][1] - points_of_ocr[1][1])**2))
+            x_of_ocr = int(min(points_of_ocr[:,0]))
+            y_of_ocr = int(min(points_of_ocr[:,1]))
+                            
+
+            cropped_img_of_ocr = cropped_img[y_of_ocr: y_of_ocr+height_of_ocr, x_of_ocr: x_of_ocr+width_of_ocr]
+            pre_img_of_ocr = Image.fromarray(cropped_img_of_ocr)
+            # pre_img_of_ocr.show()
+            ocrResult = self.viet.predict(pre_img_of_ocr)
+            ocrResult
+            results.append(ocrResult)
+        return results
+    
+
+    def _ocr_from_pdf(self, pdf_path):
+        dict_results = {}
         pages = convert_from_path(pdf_path, poppler_path=r'D:\poppler-23.05.0\Library\bin')
         folder_name = os.path.join('pdf', os.path.basename(pdf_path)[:-4])
         for idx, page in enumerate(pages):
             os.makedirs(folder_name, exist_ok=True)
             page.save(os.path.join(folder_name, 'page_{}.jpg'.format(idx)), 'JPEG')
             try:
+                # Đọc ảnh
                 image = cv2.imread(os.path.join(folder_name, 'page_{}.jpg'.format(idx)))
                 boxs = self.paddle.ocr(image, cls=False, det=True, rec=False)
-
-                # Đọc ảnh
-                # img = Image.open(os.path.join(folder_name, 'page_{}.jpg'.format(idx)))
-
-                # Khởi tạo một đối tượng vẽ trên ảnh
-                # draw = ImageDraw.Draw(img)
-                
-                #sắp xếp lại ocr
+#                 # Khởi tạo một đối tượng vẽ trên ảnh
+                img = Image.open(os.path.join(folder_name, 'page_{}.jpg'.format(idx)))
+                draw = ImageDraw.Draw(img)
+#                #sắp xếp lại ocr
                 boxs=self.sort(boxs)
-                
 #=========================                Phân đoạn văn bản               =============================
                 new_boxs = self.text_partition(boxs)
-
-                for box in new_boxs:
-                    # polygons = box        
+                results = []
+                for box in new_boxs:       
                     # Vẽ hình chữ nhật bao quanh từ hoặc đoạn văn bản
-                    # for i in range(0, len(polygons)):
-                    #     draw.line(polygons[i] + (polygons[(i + 1) % len(polygons)]), fill='red', width=3)
-
+                    for i in range(0, len(box)):
+                        draw.line(box[i] + (box[(i + 1) % len(box)]), fill='red', width=3)
                     # Chuyển tọa độ về kiểu int
                     points = np.array(box, dtype=np.int32)
                     # Tính toán kích thước của hình chữ nhật
                     width = int(np.sqrt((points[1][0] - points[0][0])**2 + (points[1][1] - points[0][1])**2))
                     height = int(np.sqrt((points[2][0] - points[1][0])**2 + (points[2][1] - points[1][1])**2))
-
                     # Tọa độ góc trái trên của hình chữ nhật
                     x = int(min(points[:,0]))
                     y = int(min(points[:,1]))
                     # Cắt ảnh theo vùng đã tính toán được
-                    # print(x,y, height, width)
-                    cropped_img = image[y: y+height, x: x+width]
-                    pre_img = Image.fromarray(cropped_img)  
-                    # pre_img.show()
-#==================              Cắt ảnh rồi đem predict             ===========================                        
-                    boxs_of_ocr=self.paddle.ocr(cropped_img,cls=False, det=True, rec=False)
-                    # print(boxs_of_ocr)
+                    cropped_img = image[y: y+height, x: x+width] 
 
-                    draw123 = ImageDraw.Draw(pre_img)   
-                    for box_of_ocr in boxs_of_ocr[0]:          
-                        polygons123 = box_of_ocr        
-
-                        for i in range(0, len(polygons123)):
-                            draw123.line(polygons123[i] + (polygons123[(i + 1) % len(polygons123)]), fill='red', width=3)
-                        points_of_ocr = np.array(box_of_ocr, dtype=np.int32)
-
-                        width_of_ocr = int(np.sqrt((points_of_ocr[1][0] - points_of_ocr[0][0])**2 + (points_of_ocr[1][1] - points_of_ocr[0][1])**2))
-                        height_of_ocr = int(np.sqrt((points_of_ocr[2][0] - points_of_ocr[1][0])**2 + (points_of_ocr[2][1] - points_of_ocr[1][1])**2))
-                        x_of_ocr = int(min(points_of_ocr[:,0]))
-                        y_of_ocr = int(min(points_of_ocr[:,1]))
-                        cropped_img_of_ocr = image[y_of_ocr: y_of_ocr+height_of_ocr, x_of_ocr: x_of_ocr+width_of_ocr]
-                        pre_img_of_ocr = Image.fromarray(cropped_img_of_ocr)
-
-                        ocrResult = self.viet.predict(pre_img_of_ocr)
-                        results.append(ocrResult)
-
-                    pre_img.show()
+#==================              Cắt ảnh rồi đem predict             ===========================        
+#                 
+                    results.append(self.ocr_by_vietocr(cropped_img)  )
+                dict_results[idx+1] = results
+                # print(dict_results)
+                img.show()
+                # break
             except Exception as e:
-                results.append(e)
-            break
-            # img.show()
-        # print(results)
-        return results
+                dict_results[idx]=e
+                # continue
+                
+        return dict_results
 ocr = OCR()
 readtext=ocr._ocr_from_pdf(r"C:\Users\ASUS\Downloads\VD1.pdf")
 # readtext=ocr._ocr_from_pdf(r"C:\Users\ASUS\Downloads\NLNKHMT12.pdf")
 print(readtext)
-
+# C:\Users\ASUS/.cache\torch\hub\checkpoints\vgg19_bn-c79401a0.pth
 
  
 # #================================================================================================================================================
